11a1fcaeb5563ba27d5e595cfc790b80f04a4ecb
Muhammad Ali Hasan <alihxn23@gmail.com>
2024-12-27 12:43:00 -0600

    add directory-tree and fuzzy-sort for directory search

--- index.js
+++ index.js
@@ -4,14 +4,16 @@ import { checkbox, input, search } from "@inquirer/prompts";
 import fs from "fs";
 import nodemon from "nodemon";
 import YAML from "yaml";
+import fuzzysort from "fuzzysort";
+import directoryTree from "directory-tree";
 
 // calling esbuild
 // node ./${esbuild.js filename} --dir {artifacts_directory}
 
-// artifacts directory: ${pwd}/.aws-sam/build/${FunctionName}
 const artifacts_directory = `${process.cwd()}/.aws-sam/build`;
 console.log(artifacts_directory);
 
+// parse template.yaml to extract lambda functions
 const file = fs.readFileSync("./template.yaml", "utf8");
 const yaml = YAML.parse(file, { logLevel: "error" });
 const functionNames = [];
@@ -20,26 +22,41 @@ Object.entries(yaml.Resources).forEach(([k, v]) => {
     functionNames.push(k);
   }
 });
-// console.log(functionNames);
 
+// select lambda functions to hot-reload
 const answer = await checkbox({
   message: "which functions do you want to watch for hot-reload?",
   choices: [...functionNames.map((f) => ({ value: f }))],
   required: true,
 });
 
-const esbuildPath = await input({ message: "esbuild path", required: true });
-console.log("esbuild path", esbuildPath);
+// directory tree for the project
+const t = directoryTree("./", { exclude: /node_modules/ }).children.map(
+  (e) => e.path
+);
 
-// console.log("answer", answer);
+// path of esbuild file
+const esbuildPath = await search({
+  message: "esbuild path",
+  source: async (input, { signal }) => {
+    if (!input) {
+      return [];
+    }
+
+    const results =
+      fuzzysort.go(input, t)?.map((e) => ({ value: e.target })) ?? [];
+
+    return results;
+  },
+});
 
 const commands = answer.map(
-  (a) => `node ./esbuild.js --dir ${artifacts_directory}/${a}`
+  (a) => `node ./${esbuildPath} --dir ${artifacts_directory}/${a}`
 );
-console.log("commands", commands);
 
-// nodemon({ exec: commands.join("; ") });
+// start nodemon
+nodemon({ exec: commands.join("; "), ext: "js ts" });
 
-// nodemon.on("restart", (files) => {
-//   console.log("app restarted due to: ", files);
-// });
+nodemon.on("restart", (files) => {
+  console.log("app restarted due to: ", files);
+});
